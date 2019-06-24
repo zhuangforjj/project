81b5dddba6c1f0014c3559400403167c7cd270e0
zhuangforjj <305066116@qq.com>
2019-06-24 23:32:48 +0800

    Initial commit

---
+++ src/com/jj/hello_world.java
@@ -0,0 +1,7 @@
+package com.jj;
+
+public class hello_world {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
---
+++ untitled01/src/com/jj/hello_world.java
@@ -0,0 +1,14 @@
+package com.jj;
+
+public class hello_world {
+    //psvm模版
+    public static void main(String[] args){
+        //sout模版
+        System.out.println("hello world!!");
+        //fori模版
+        for (int i = 0; i <10 ; i++) {
+
+        }
+    }
+
+}
